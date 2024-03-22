8ee5ebafbd66d6bf982a7233aa49dd168bc368b2
Logan Foote <89160069+loganfoote@users.noreply.github.com>
2024-03-21 17:46:31 -0700

    Added uncertainties to responsivity

--- citkid/responsivity/fitter.py
+++ citkid/responsivity/fitter.py
@@ -5,8 +5,9 @@ from .guess import guess_p0_responsivity_int, get_bounds_responsivity_int
 from .plot import plot_responsivity_int
 from .data_io import make_fit_row
 
-def fit_responsivity_int(power, x, f1, guess = None, guess_nfit = 3,
-                         return_dataframe = False, plotq = False):
+def fit_responsivity_int(power, x, f1, x_err = None, guess = None,
+                         guess_nfit = 3, return_dataframe = False,
+                         plotq = False):
     """
     Fits x versus power data to the integrated responsivity equation.
     The fitter works best if there are at least three data points at P >> P_0.
@@ -16,7 +17,10 @@ def fit_responsivity_int(power, x, f1, guess = None, guess_nfit = 3,
     power (array-like): array of blackbody powers in W
     x (array-like): array of fractional frequency shifts in Hz / Hz. This must
         be scaled close to x(P = 0) = 0 for the initial guess to work well
+    x_err ()
     f1 (float): frequency at P = 0 that was used to calculate x
+    x_err (None or array-like): if not None, uncertainty on x for the fitter.
+        if None, fits without uncertainty
     guess (list or None): If not None, guess is used as the initial guess
         [R0, P0, c]
     guess_nfit (int): number of high-power (P >> P_0) points in the data for the
@@ -35,6 +39,8 @@ def fit_responsivity_int(power, x, f1, guess = None, guess_nfit = 3,
     """
     ix = np.argsort(power)
     power, x = power[ix], x[ix]
+    if x_err is not None:
+        x_err = x_err[ix]
     # Initial guess
     if guess is not None:
         p0 = guess
@@ -42,19 +48,35 @@ def fit_responsivity_int(power, x, f1, guess = None, guess_nfit = 3,
     else:
         p0, bounds = guess_p0_responsivity_int(power, x, guess_nfit = guess_nfit)
     # Fit
+    if x_err is None:
+        sigma = None
+        p00 = p0
+    else:
+        sigma = x_err * 1e6
+        try:
+            p00, _ = curve_fit(responsivity_int_for_fitter, np.log(power),
+                               x * 1e6, sigma = None, p0 = p0,
+                               bounds = bounds)
+           # To fit with sigma, the initial guess must be really good, so
+           # update the initial guess with curve_fit without sigma
+        except:
+            p00 = p0
     try:
         popt, pcov = curve_fit(responsivity_int_for_fitter, np.log(power),
-                               x * 1e6, p0 = p0, bounds = bounds)
+                               x * 1e6, sigma = sigma, p0 = p00,
+                               bounds = bounds, absolute_sigma = True)
         perr = np.sqrt(np.diag(pcov))
         p0[0], popt[0], perr[0] = p0[0] * 1e9  , popt[0] * 1e9  , perr[0] * 1e9
         p0[1], popt[1], perr[1] = p0[1] * 1e-16, popt[1] * 1e-16, perr[1] * 1e-16
         # p0[2], popt[2], perr[2] = p0[2] * 1e-8 , popt[2] * 1e-8 , perr[2] * 1e-8
-    except:
+    except Exception as e:
+        p0[0] = p0[0] * 1e9
+        p0[1] = p0[1] * 1e-16
         popt = [np.nan, np.nan, np.nan]
         perr = [np.nan, np.nan, np.nan]
     # Plot
     if plotq:
-        fig, ax = plot_responsivity_int(power, x, popt, p0)
+        fig, ax = plot_responsivity_int(power, x, x_err, popt, p0)
     else:
         fig, ax = None, None
     # Determine f0
--- citkid/responsivity/plot.py
+++ citkid/responsivity/plot.py
@@ -2,13 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from .funcs import responsivity_int
 
-def plot_responsivity_int(power, x, popt, p0):
+def plot_responsivity_int(power, x, x_err, popt, p0):
     """
     Plots the fit and initial guess to responsivity_int
 
     Parameters:
     power (array-like): array of blackbody powers in W
-    x (array-like): array of fractional frequency shifts in Hz / Hz.
+    x (array-like): array of fractional frequency shifts in Hz / Hz
+    x_err (None or array-like): error bars for the plot, or None to plot without
+        error bars
     p0 (list): initial guess parameters [R0_guess, P0_guess, c_guess]
     popt (list): fit parameters [R0, P0, c]
 
@@ -19,9 +21,14 @@ def plot_responsivity_int(power, x, popt, p0):
     ax.set_ylabel(r'$df / f$ (kHz / GHz)')
     ax.set_xlabel(r'Power (W)')
     ax.set_xscale('log')
-
-    ax.plot(power, x * 1e6, marker = '.', color = plt.cm.viridis(0),
-            linestyle = '', label = 'Data')
+    if x_err is None:
+        ax.plot(power, x * 1e6, marker = '.', color = plt.cm.viridis(0),
+                linestyle = '', label = 'Data')
+    else:
+        ax.errorbar(power, x * 1e6, yerr = x_err * 1e6, marker = '.',
+                    color = plt.cm.viridis(0), linestyle = '', label = 'Data')
+        # ax.plot(power, x * 1e6, marker = '.', color = 'b',
+        #         linestyle = '', label = 'Data')
 
     psamp = np.geomspace(min(power), max(power), 200)
     ysamp = responsivity_int(psamp, *popt)
