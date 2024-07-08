5c7633f998c1c25f550c8346734c81f8fe7d58d6
Logan Foote <89160069+loganfoote@users.noreply.github.com>
2024-07-08 11:49:00 -0700

    added function to extract common mode timestreams

--- citkid/noise/pca.py
+++ citkid/noise/pca.py
@@ -13,6 +13,10 @@ def pca(y, n_components = 5):
     Returns:
     z (np.array): array of timestream datasets corresponding to y, with
         principal components removed
+    U (np.array): SVD unitary array, scaled output of np.linalg.svd
+    S (np.array): SVD vector with singular values, scaled output of
+        np.linalg.svd
+    Vh (np.array): SVD unitary array, scaled output of np.linalg.svd
     """
     # Normalize input data
     y = np.array(y).T
@@ -22,13 +26,33 @@ def pca(y, n_components = 5):
     y_normalized /= std_dev
     y_normalized = y_normalized.T
     # Perform SVD
-    U, S, eigenvectors = np.linalg.svd(y_normalized, full_matrices = False)
+    U, S, Vh = np.linalg.svd(y_normalized, full_matrices = False)
     S_rmvd = S.copy()
     S_rmvd[0:n_components] = 0. # set the components to remove to zero
     # Reconstruct data with modes removed
-    z_normalized = (U * S_rmvd) @ eigenvectors
+    z_normalized = (U * S_rmvd) @ Vh
     # Remove normalization
     z = ((z_normalized.T * std_dev) + mean).T
-    # Get eigenvalues
-    eigenvalues = (U * S).T * std_dev + mean
-    return z
+    # un-normalize S
+    S = S * std_dev + mean
+    return z, (U, S, Vh)
+
+def get_common_mode(U, S, Vh, resonator_index, mode_index):
+    """
+    Extract a single common mode timestream for the given resonator after
+    performing a principal component analysis using the SVD method.
+
+    Parameters:
+    U (np.array): SVD unitary array, scaled output of np.linalg.svd
+    S (np.array): SVD vector with singular values, scaled output of
+        np.linalg.svd
+    Vh (np.array): SVD unitary array, scaled output of np.linalg.svd
+    resonator_index (int): resonator index to scale the common mode
+    mode_index (int): index of the common mode to extract
+
+    Returns:
+    m (np.array): common mode timestream for given resonator and mode index
+    """
+    A = (U * S[mode_index])
+    m = A[resonator_index, mode_index] * Vh[mode_index, :]
+    return m
