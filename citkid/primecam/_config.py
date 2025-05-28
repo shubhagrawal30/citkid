48dab420aecaf86c67545abeea8a56ca58ba58e4
Shubh Agrawal (aurora) <shubh@sas.upenn.edu>
2025-05-28 12:06:21 -0400

    update gitignorew

--- citkid/primecam/_config.py
+++ citkid/primecam/_config.py
@@ -1,6 +1,6 @@
 import os
 
-xilinx_ip = os.getenv("XILINX_IP", XILINX_IP)
+xilinx_ip = os.getenv("XILINX_IP", "192.168.2.99")
 xilinx_sshport = 22
-xilinx_username = os.getenv("XILINX_USERNAME", XILINX_USERNAME)
-xilinx_password = os.getenv("XILINX_PASSWORD", XILINX_PASSWORD)
+xilinx_username = os.getenv("XILINX_USERNAME", "xilinx")
+xilinx_password = os.getenv("XILINX_PASSWORD", "xilinx")
